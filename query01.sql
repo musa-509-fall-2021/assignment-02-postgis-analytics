f38cb46e80aeb91604dad5922385a2a500c9db0c
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-28 21:01:24 -0400

    Update query01.sql

--- query01.sql
+++ query01.sql
@@ -7,7 +7,7 @@
 
 create index septa_bus_stops__the_geom__32129__idx
     on septa_bus_stops
-    using GiST (ST_Transform(the_geom, 32129));
+    using GiST (ST_Transform(ST_SetSRID(ST_Makepoint(stop_lon,stop_lat),4326), 32129));
 
 
 with septa_bus_stop_block_groups as (
@@ -17,26 +17,27 @@ with septa_bus_stop_block_groups as (
     from septa_bus_stops as s
     join census_block_groups as bg
         on ST_DWithin(
-            ST_Transform(s.the_geom, 32129),
+            ST_Transform(ST_SetSRID(ST_Makepoint(s.stop_lon,s.stop_lat),4326), 32129),
             ST_Transform(bg.the_geom, 32129),
             800
         )
 ),
-
 septa_bus_stop_surrounding_population as (
     select
-        stop_id,
-        sum(population) as estimated_pop_800m
+        s.stop_id,
+        sum(p.total) as estimated_pop_800m
     from septa_bus_stop_block_groups as s
-    join census_population as p using (geo_id)
+    join census_population as p 
+  	 on s.geo_id = p.id
     group by stop_id
 )
 
 select
-    stop_name,
-    estimated_pop_800m,
-    the_geom
-from septa_bus_stop_surrounding_population
-join septa_bus_stops using (stop_id)
+    s.stop_name,
+    b.estimated_pop_800m,
+    ST_Setsrid(ST_makepoint(s.stop_lon,s.stop_lat),4326) as the_geom
+from septa_bus_stop_surrounding_population as b
+join septa_bus_stops as s 
+on s.stop_id = b.stop_id
 order by estimated_pop_800m desc
 limit 1
