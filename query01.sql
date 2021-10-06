901a122df84f27245255953d28b7ec15d382108d
yzy2000 <yzy2000@upenn.edu>
2021-10-06 15:04:47 -0400

    query1

--- query01.sql
+++ query01.sql
@@ -4,12 +4,19 @@
   of the 800 meter buffer.
 */
 
+alter table septa_bus_stops
+add column the_geom geometry(Geometry,32129);
 
-create index septa_bus_stops__the_geom__32129__idx
-    on septa_bus_stops
-    using GiST (ST_Transform(the_geom, 32129));
+update septa_bus_stops
+set the_geom = ST_transform(ST_SetSRID(st_makepoint(stop_lon,stop_lat),4326),32129);
 
+update census_block_groups
+set geom= ST_transforms(ST_SetSRID(geom,4326),32129);
 
+create index septa_stops_the_geom_32129_idx
+	on septa_bus_stops
+	using GiST(ST_Transform(the_geom,32129));
+	
 with septa_bus_stop_block_groups as (
     select
         s.stop_id,
@@ -18,20 +25,18 @@ with septa_bus_stop_block_groups as (
     join census_block_groups as bg
         on ST_DWithin(
             ST_Transform(s.the_geom, 32129),
-            ST_Transform(bg.the_geom, 32129),
+            ST_Transform(bg.geom, 32129),
             800
         )
 ),
-
 septa_bus_stop_surrounding_population as (
     select
         stop_id,
-        sum(population) as estimated_pop_800m
+        sum(total) as estimated_pop_800m
     from septa_bus_stop_block_groups as s
-    join census_population as p using (geo_id)
+    join census_population as p on s.geo_id = p.id
     group by stop_id
 )
-
 select
     stop_name,
     estimated_pop_800m,
