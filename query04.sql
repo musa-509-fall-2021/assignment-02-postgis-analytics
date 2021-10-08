79cd5612a7b03eea98d44e117a86bd2d6bbda9ca
hlechuan <89944391+hlechuan@users.noreply.github.com>
2021-10-07 22:47:27 -0400

    initial response

---
+++ query04.sql
@@ -0,0 +1,16 @@
+alter table septa_bus_shapes
+    add column the_geom geometry(Geometry, 32129);
+
+update septa_bus_shapes
+    set the_geom = ST_Transform(ST_SetSRID(st_makepoint(
+	               shape_pt_lon, shape_pt_lat),4326),32129);
+
+with septa_bus_lines as (
+	SELECT shape_id, 
+      ST_MakeLine(the_geom) As linepath
+	FROM septa_bus_shapes As bl
+	GROUP BY shape_id)
+
+select shape_id as trip_headsign,
+       ST_Length(linepath) as trip_length
+	   FROM septa_bus_lines
