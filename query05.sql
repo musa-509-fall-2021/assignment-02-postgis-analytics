0e791aab06e4f4b05520837145837db9749d25a7
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-10-10 01:30:37 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -8,11 +8,23 @@ With joined as (
 	from census_block_groups as c
 	left join septa_bus_stops as s
 		on st_contains(st_setsrid(c.geom,4326),s.the_geom))
+	, accessible as (
+	select block_name,
+			count(*) as num_bus_stops_accessible
+	from joined
+	where wheelchair_boarding = '1'
+	group by 1
+	)
+	, unaccessible as (
+	select block_name,
+			count(*) as num_bus_stops_unaccessible
+	from joined
+	where wheelchair_boarding = '2'
+	group by 1)
 
-select block_name,
-		count(*) as num_bus_stops_accessible
-from joined
-where wheelchair_boarding = '1'
-group by 1
-order by 2 DESC
+select a.*,u.num_bus_stops_unaccessible
+from accessible as a
+join unaccessible as u
+	on a.block_name = u.block_name
+order by a.num_bus_stops_accessible desc
 limit 5
