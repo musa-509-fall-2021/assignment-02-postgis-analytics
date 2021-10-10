230627666a5f781d9fb79ce349d3df62b2341a4d
Mjumbe Poe <mjumbewu@gmail.com>
2021-10-10 17:02:56 -0400

    Update query01 to use default field names and be more explicit

--- query01.sql
+++ query01.sql
@@ -5,7 +5,15 @@
 */
 
 
-create index septa_bus_stops__the_geom__32129__idx
+alter table septa_bus_stops
+    add column if not exists the_geom geometry(Point, 4326);
+
+
+update septa_bus_stops
+  set the_geom = ST_SetSRID(ST_MakePoint(stop_lon, stop_lat), 4326);
+
+
+create index if not exists septa_bus_stops__the_geom__32129__idx
     on septa_bus_stops
     using GiST (ST_Transform(the_geom, 32129));
 
@@ -26,9 +34,9 @@ with septa_bus_stop_block_groups as (
 septa_bus_stop_surrounding_population as (
     select
         stop_id,
-        sum(population) as estimated_pop_800m
+        sum(total) as estimated_pop_800m
     from septa_bus_stop_block_groups as s
-    join census_population as p using (geo_id)
+    join census_population as p on (s.geo_id = p.id)
     group by stop_id
 )
 
