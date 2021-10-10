3ba7087d5ce273b746ab9f04b8b90b8840b4e7fa
johnatawnclementawn <johnathan.clementi1@gmail.com>
2021-10-10 18:56:19 -0400

    Functioning query, but results don't appear... hmmm

--- query06.sql
+++ query06.sql
@@ -10,18 +10,46 @@
 --   )
 --   ```
 
-with accessibleBuffers as (
-	select stop_id, stop_name, st_buffer(st_transform(the_geom,32129),322) as the_geom
+with accessibleStops as (
+	select stop_id, the_geom
 	from septa_bus_stops
 	where wheelchair_boarding = 1
 ), 
-accessibleProperties as (
-	select stop_id, count(*)
+inaccessibleStops as (
+	select stop_id, the_geom
+	from septa_bus_stops
+	where wheelchair_boarding = 0 OR wheelchair_boarding = 2
+), 
+accessibleBuffers as (
+	select stop_id, st_buffer(st_transform(the_geom,32129),322) as the_geom
+	from accessibleStops
+), 
+accessibilityIDX as (
+	select stop_id, count(*) as accessibility_metric
 	from accessibleBuffers buf
 	left join pwd_parcels pcls
 	on st_contains(buf.the_geom, pcls.the_geom)
 	group by stop_id
+), 
+accessibilityIDXgeom as (
+	select a.stop_id as stop_id, a.accessibility_metric as accessibility_metric, st_transform(s.the_geom,32129) as the_geom
+	from accessibilityIDX a
+	left join septa_bus_stops s
+	on a.stop_id = s.stop_id
 )
 
-select * 
-from accessibleProperties
+select nbhd.name as neighborhood_name, 
+		sum(aidxgeom.accessibility_metric) as accessibility_metric,
+		count(accStp.stop_id) as num_bus_stops_accessible,
+		count(inAccStp.stop_id) as num_bus_stops_inaccessible
+from neighborhoods_philadelphia nbhd
+	left join accessibilityIDXgeom aidxgeom 
+	on st_contains(st_transform(nbhd.the_geom,32129), aidxgeom.the_geom)
+	left join accessibleStops accStp 
+	on st_contains(st_transform(nbhd.the_geom,32129), st_transform(accStp.the_geom,32129))
+	left join inaccessibleStops inAccStp
+	on st_contains(st_transform(nbhd.the_geom,32129), st_transform(inAccStp.the_geom,32129))
+group by nbhd.name
+order by accessibility_metric desc
+limit 5;
+
