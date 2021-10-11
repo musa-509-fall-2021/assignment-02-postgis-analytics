90e05ce48b6d55ce54b62ee16a5aedd5860cd7f7
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-10-11 00:07:47 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -1,3 +1,7 @@
+create index if not exists wheelchair_boarding__idx
+    on septa_bus_stops
+    using HASH (wheelchair_boarding);
+  
 With joined as (
 	select 
 		c.geoid10 as block_name,
@@ -8,23 +12,18 @@ With joined as (
 	from census_block_groups as c
 	left join septa_bus_stops as s
 		on st_contains(st_setsrid(c.geom,4326),s.the_geom))
-	, accessible as (
-	select block_name,
+
+select a.*,u.num_bus_stops_unaccessible
+from (select block_name,
 			count(*) as num_bus_stops_accessible
 	from joined
 	where wheelchair_boarding = '1'
-	group by 1
-	)
-	, unaccessible as (
-	select block_name,
+	group by 1) as a
+join (select block_name,
 			count(*) as num_bus_stops_unaccessible
 	from joined
 	where wheelchair_boarding = '2'
-	group by 1)
-
-select a.*,u.num_bus_stops_unaccessible
-from accessible as a
-join unaccessible as u
+	group by 1) as u
 	on a.block_name = u.block_name
 order by a.num_bus_stops_accessible desc
 limit 5
