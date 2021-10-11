00b5d3d549e74673ce3887637e5506fa609a3d09
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-10-10 21:51:09 -0400

    Update query08.sql

--- query08.sql
+++ query08.sql
@@ -1,19 +1,22 @@
 /*
 data source: https://opendataphilly.org/dataset/philadelphia-universities-and-colleges
-
+query9: With a query, find out how many census block groups Penn's main campus fully contains. Discuss which dataset you chose for defining Penn's campus.
 */
 
 
-with upenn as (
-	select *
-	from university 
-	where NAME = 'University of Pennsylvania')
-	, joined as (
+select count(*) as count_block_groups
+from (
 	select c.geoid10,c.geom
-	from upenn as u
+	from (
+		select *
+		from university 
+		where NAME = 'University of Pennsylvania') as u
 	join census_block_groups as c
 		on st_contains(st_setsrid(c.geom,4326),u.geom)
-	)
+	) as joined
 
-select count(*)
-from joined
+
+/*
+count_block_groups
+191
+*/
