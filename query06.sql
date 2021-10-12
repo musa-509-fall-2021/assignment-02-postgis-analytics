45d0dd8438e1d11e35e49809f947e14d518fe476
hlechuan <89944391+hlechuan@users.noreply.github.com>
2021-10-11 22:17:16 -0400

    initial response

---
+++ query06.sql
@@ -0,0 +1,19 @@
+with neighborhood_acc_count as (
+	SELECT p.listname neighborhood_name, p.shape_area,
+       count(*) filter(where wheelchair_boarding = 1) as num_bus_stops_accessible,
+       count(*) filter(where wheelchair_boarding = 2) as num_bus_stops_inaccessible
+    FROM neighborhoods_philadelphia as p
+    JOIN septa_bus_stops as s
+    ON ST_Contains(ST_transform(p.geom, 32129), s.the_geom)
+GROUP BY 1,2)
+
+select neighborhood_name, num_bus_stops_accessible, num_bus_stops_inaccessible,
+       (num_bus_stops_accessible/(num_bus_stops_inaccessible+1))/shape_area as accessibility_metric
+	   FROM neighborhood_acc_count
+	   ORDER BY accessibility_metric 
+	   DESC
+	   LIMIT 5
+
+/*
+  Hawthorne, Francisville, Walnut Hill, Fairmount, West Passyunk
+*/
