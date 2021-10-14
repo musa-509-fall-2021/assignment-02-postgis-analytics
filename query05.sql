9e18c3e16d39596d83cbadc88f6eb4b9b80c1688
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-10-14 17:51:47 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -2,28 +2,35 @@ create index if not exists wheelchair_boarding__idx
     on septa_bus_stops
     using HASH (wheelchair_boarding);
   
-With joined as (
-	select 
-		c.geoid10 as block_name,
-		st_setsrid(c.geom,4326),
-		s.stop_name,
-		s.wheelchair_boarding,
-		s.the_geom
-	from census_block_groups as c
-	left join septa_bus_stops as s
-		on st_contains(st_setsrid(c.geom,4326),s.the_geom))
+with scores as (select stop_name,the_geom,wheelchair_boarding,
+			case  when parent_station is null and wheelchair_boarding = '1'  then 1
+				when parent_station is not null and wheelchair_boarding = '1' then 2
+				when parent_station is not null and (wheelchair_boarding = '1' or wheelchair_boarding is null ) then 1
+			end as scores
+	from septa_bus_stops)
 
-select a.*,u.num_bus_stops_unaccessible
-from (select block_name,
+,joined as (select 
+		n.name as neighborhood_name,
+		s.*
+	from neighborhoods_philadelphia as n
+	left join scores as s
+		on st_contains(st_transform(n.geom,4326),s.the_geom)
+			)
+	
+select a.*,u.num_bus_stops_unaccessible,t.total_score
+from (select neighborhood_name,
 			count(*) as num_bus_stops_accessible
 	from joined
 	where wheelchair_boarding = '1'
 	group by 1) as a
-join (select block_name,
+join (select neighborhood_name,
 			count(*) as num_bus_stops_unaccessible
 	from joined
 	where wheelchair_boarding = '2'
 	group by 1) as u
-	on a.block_name = u.block_name
-order by a.num_bus_stops_accessible desc
-limit 5
+	on a.neighborhood_name = u.neighborhood_name
+join (select neighborhood_name,
+	 	sum(scores) as total_score
+	 from joined
+	 group by 1) as t
+	 on t.neighborhood_name = u.neighborhood_name
