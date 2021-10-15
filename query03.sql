1c37183419b630481157e3d9bfafcf5bd13d8331
Jiali Yao <jialiyao@upenn.edu>
2021-10-15 10:53:36 -0400

    Update query03.sql

--- query03.sql
+++ query03.sql
@@ -4,9 +4,26 @@ The final result should give the parcel address, bus stop name, and distance apa
 Order by distance (largest on top).
 */
 
-select stop_name,
-address,
-min(st_distance(s.the_geom, p.the_geom) as distance
-from pwd_parcels as p
-cross join
-  septa_bus_stops as s
+
+create index phl_pwd_parcels_idx
+  on phl_pwd_parcels
+  using gist (geom);
+
+create index septa_bus_stops_idx
+  on septa_bus_stops
+  using gist (the_geom);
+
+select
+      p.address,
+      a.stop_name,
+      a.distance * 111000 as distance_m
+from
+      phl_pwd_parcels as p
+cross join lateral(
+  select stop_name,
+         the_geom,
+         p.geom <->s.the_geom as distance
+  from septa_bus_stops s
+  order by distance
+  limit 1) a
+    order by distance_m desc
