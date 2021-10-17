90e34332b03cab91d96c30d9bde12fff9e4e35f1
Jiali Yao <jialiyao@upenn.edu>
2021-10-17 10:51:46 -0400

    Update query03.sql

--- query03.sql
+++ query03.sql
@@ -7,16 +7,15 @@ Order by distance (largest on top).
 /* 03-10 using PostgreSQL*/
 
 
-
 select address,
        stop_name,
        distance_m*111000 as distance_m
 from phl_pwd_parcels p
     cross join lateral (
         select stop_name,
-        p.geo<-> b.geo distance_m
+        p.geom<-> b.the_geomgeo distance_m
         from septa_bus_stops b
-        order by 2
+        order by distance_m
         limit 1
     ) a
-order by 3 desc
+order by distance_m desc
