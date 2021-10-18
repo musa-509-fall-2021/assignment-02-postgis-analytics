83b02f12eee6e1909033ebfc7e4e0b17e51681a1
bensh_ <12397895+bensh3@users.noreply.github.com>
2021-10-18 17:00:56 -0400

    update query 3

--- query03.sql
+++ query03.sql
@@ -6,6 +6,10 @@
 
 */
 
+create index if not exists septa_bus_stops__the_geom__32129__idx
+    on septa_bus_stops
+    using GiST (ST_Transform(the_geom, 32129));
+
 alter table phl_pwd_parcels
   add column if not exists the_geom geometry(MultiPolygon,4326);
 
@@ -16,13 +20,13 @@ create index if not exists phl_pwd_parcels__the_geom__32129__idx
     on phl_pwd_parcels
     using GiST (ST_Transform(the_geom, 32129));
 
-select distinct on (pwd.address) pwd.address,
-  s.stop_name, ST_Distance(pwd.the_geom,s.the_geom) as dist
+select pwd.address, cs.stop_name, cs.dist_m
   from phl_pwd_parcels as pwd
-  join septa_bus_stops as s
-    on ST_DWithin(
-      ST_Transform(pwd.the_geom, 32129),
-      ST_Transform(s.the_geom, 32129),
-      1000
-    )
+  cross join lateral (
+      select s.stop_name, pwd.the_geom<->s.the_geom dist_m
+      from septa_bus_stops as s
+      order by dist_m
+      limit 1
+  ) as cs
+order by dist_m desc
  
