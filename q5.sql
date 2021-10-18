c1de1c12781974a9103ffd4c49e1a8c9a678c04d
yzy2000 <89943485+yzy2000@users.noreply.github.com>
2021-10-18 10:20:37 -0400

    Update q5.sql

--- q5.sql
+++ q5.sql
@@ -1,4 +1,4 @@
-with accessible_bus_stops as (
+with bus_stops_accessible as (
      select
         stop_name,
         wheelchair_boarding,
@@ -12,14 +12,14 @@ with accessible_bus_stops as (
 	 join septa_bus_stops as bs
 	 on st_Within(ST_Transform(ph.geom,4326), ST_Transform(bs.the_geom,4326))),
 		
-	 bus_neighbors_access as(
+	 bus_access as(
 	    select name as neighborhood_name,
-               count(*) filter(where wheelchair_boarding = 1) as num_bus_stops_accessible,
-			   count(*) filter(where wheelchair_boarding = 2) as num_bus_stops_inaccessible,
+               count(*) filter(where wheelchair_boarding = 1) as accessible,
+			   count(*) filter(where wheelchair_boarding = 2) as nonaccessible,
 			   area_size
 		  from bus_neighbors
 		  group by neighborhood_name, area_size)
 		
-select neighborhood_name, num_bus_stops_accessible, num_bus_stops_inaccessible,
-       num_bus_stops_accessible/area_size as accessibility_metric
-	   from bus_neighbors_access
+select neighborhood_name, accessible, nonaccessible,
+       accessible/area_size as accessibility_metric
+	   from bus_access
